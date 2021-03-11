aac67306d1fc9d3e552bebb9394d77be9ba766d4
Yaccov <yaccov@ipi.co.il>
2021-03-11 16:14:35 +0200

    Alex

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,6 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            //tyutyytyt
             Console.WriteLine("Hello World!");
             Console.ReadKey();
         }
